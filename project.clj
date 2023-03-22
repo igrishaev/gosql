f2bd63f57906e84b108b3052b2fc0badae59ece5
Ivan Grishaev <ivan@grishaev.me>
2023-03-22 16:56:42 +0300

    udpated

--- project.clj
+++ project.clj
@@ -1,14 +1,28 @@
-(defproject kek "0.1.0-SNAPSHOT"
+(defproject com.github.igrishaev/gosql "0.1.0-SNAPSHOT"
 
   :description
-  "FIXME: write description"
+  "Good old SQL driven with templates"
 
   :url
-  "http://example.com/FIXME"
+  "https://github.com/igrishaev/gosql"
+
+  :deploy-repositories
+  {"releases" {:url "https://repo.clojars.org" :creds :gpg}}
 
   :license
   {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
    :url "https://www.eclipse.org/legal/epl-2.0/"}
+
+  :release-tasks
+  [["vcs" "assert-committed"]
+   ["test"]
+   ["change" "version" "leiningen.release/bump-version" "release"]
+   ["vcs" "commit"]
+   ["vcs" "tag" "--no-sign"]
+   ["deploy"]
+   ["change" "version" "leiningen.release/bump-version"]
+   ["vcs" "commit"]
+   ["vcs" "push"]]
 
   :dependencies
   [[selmer "1.12.56"]
@@ -22,15 +36,9 @@
     :dependencies
     [[org.clojure/clojure "1.11.1"]
 
-     ;; test
-     [org.xerial/sqlite-jdbc "3.41.0.0"]
+     ;; test & bench
      [mount "0.1.17"]
-
-     ;; bench
-     [org.postgresql/postgresql "42.5.3"]
+     [org.xerial/sqlite-jdbc "3.41.0.0"]
      [com.layerware/hugsql-core "0.5.3"]
      [com.layerware/hugsql-adapter-next-jdbc "0.5.3"]
-
-     [com.github.seancorfield/honeysql "2.4.980"]
-
-     ]}})
+     [com.github.seancorfield/honeysql "2.4.980"]]}})
